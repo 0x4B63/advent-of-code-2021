88b341ecaa46fbedafbe9a09df92f41343c2fb0e
0x4b63 <0x4b63@protonmail.com>
2021-12-01 14:31:35 -0800

    Cleaned up variables.

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -6,13 +6,11 @@ pub enum AdventError {
     Parse(num::ParseIntError),
 }
 
-#[allow(unused_assignments)]
 pub fn solve_part1(inputfile: &str) -> Result<usize, AdventError> {
     let file = File::open(inputfile).map_err(AdventError::Io)?;
     let mut reader = BufReader::new(file);
     let mut line = String::new();
     let mut prev: usize = 0;
-    let mut curr: usize = 0;
     let mut output: usize = 0;
 
     loop {
@@ -21,7 +19,7 @@ pub fn solve_part1(inputfile: &str) -> Result<usize, AdventError> {
                 if count == 0 {
                     break;
                 }
-                curr = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
+                let curr = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
                 if prev < curr {
                     output += 1;
                 }
@@ -36,31 +34,26 @@ pub fn solve_part1(inputfile: &str) -> Result<usize, AdventError> {
     Ok(output - 1)
 }
 
-#[allow(unused_assignments)]
+
 pub fn solve_part2(inputfile: &str) -> Result<usize, AdventError> {
     let file = File::open(&inputfile).map_err(AdventError::Io)?;
     let mut reader = BufReader::new(file);
     let mut line = String::new();
-    let mut a: usize = 0;
-    let mut b: usize = 0;
-    let mut c: usize = 0;
-    let mut prev: usize = 0;
-    let mut curr: usize = 0;
     let mut output: usize = 0;
 
     reader.read_line(&mut line).map_err(AdventError::Io)?;
-    a = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
+    let mut a = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
     line.clear();
 
     reader.read_line(&mut line).map_err(AdventError::Io)?;
-    b = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
+    let mut b = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
     line.clear();
 
     reader.read_line(&mut line).map_err(AdventError::Io)?;
-    c = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
+    let mut c = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
     line.clear();
 
-    prev = a + b + c;
+    let mut prev = a + b + c;
 
     loop {
         match reader.read_line(&mut line) {
@@ -72,13 +65,12 @@ pub fn solve_part2(inputfile: &str) -> Result<usize, AdventError> {
                 a = b;
                 b = c;
                 c = line.trim().parse::<usize>().map_err(AdventError::Parse)?;
-                curr = a + b + c;
 
-                if prev < curr {
+                if prev < a + b + c {
                     output += 1;
                 }
 
-                prev = curr;
+                prev = a + b + c;
                 line.clear();
             },
             Err(err) => {
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ mod day01;
 
 fn main() {
     let cwd = std::env::current_dir().unwrap();
-
     let path = format!("{}{}", cwd.display(), "/src/day01/input.txt");
+
     println!("{}", day01::solve_part1(&path).unwrap());
-
-    let path = format!("{}{}", cwd.display(), "/src/day01/input.txt");
     println!("{}", day01::solve_part2(&path).unwrap());
 }
