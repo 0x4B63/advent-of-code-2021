a5729423406bc8afccb2bb0caefa7dd0ab376d96
Kc Martinez <0x4b63@protonmail.com>
2021-12-01 18:28:06 -0800

    Made output clearer.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let cwd = std::env::current_dir().unwrap();
     let path = format!("{}{}", cwd.display(), "/src/day01/input.txt");
 
-    println!("{}", day01::solve_part1(&path).unwrap());
-    println!("{}", day01::solve_part2(&path).unwrap());
+    println!("Day 01 (A): {}", day01::solve_part1(&path).unwrap());
+    println!("Day 01 (B): {}", day01::solve_part2(&path).unwrap());
 }
