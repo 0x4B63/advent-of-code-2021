8b1205d6eda1bdb8ef70c16ac80f96308e77db21
Kc Martinez <0x4b63@protonmail.com>
2021-12-20 11:04:38 -0800

    Clean up

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -10,7 +10,7 @@ pub fn solve_part1(inputfile: &str) -> Result<usize, AdventError> {
     let mut input = load(inputfile).map_err(AdventError::Io).unwrap();
     let _cmds = input[0].clone();
     input.remove(0);
-    let mut _boards: Vec<[[u8; 5]; 5]> = Vec::new();
+    let mut _boards: Vec<Vec<u8>> = Vec::new();
     
     Ok(0)
 }
